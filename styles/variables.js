7d361410098bba86dbcea52d66267de17d5c7270
Faiz Ichsan Jaya <fukuo@gmail.com>
2020-09-06 12:38:45 +0700

    Added a global style and variables

---
+++ styles/variables.js
@@ -0,0 +1,26 @@
+const fonts = {
+  bodyFont: "'Inter', Arial, sans-serif",
+  headingFont: "",
+};
+
+const colors = {
+  backgroundColor: "#f9f9f9",
+  accentColorOne: "#ff0969",
+  accentColorTwo: "#d3e2f2",
+  accentColorThree: "#0b379b",
+  bodyColor: "#666",
+  headingColor: "#333",
+  borderColor: "#ccc",
+  white: "#fff",
+  black: "#111",
+};
+
+const breakpoints = {
+  xs: 0,
+  sm: 40,
+  md: 60,
+  lg: 75,
+  xl: 90,
+};
+
+export { fonts, colors, breakpoints };
